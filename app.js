d175f3a7c837a60f054155afcbb26d316d4778b5
huangcrab <huangcrab@gmail.com>
2017-09-28 16:26:41 -0400

    master

---
+++ app.js
@@ -0,0 +1,95 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var expressValidator = require('express-validator');
+var mongojs = require('mongojs');
+var db = mongojs('customerapp', ['users']);
+var ObjectId = mongojs.ObjectId;
+var app = express();
+
+app.use(function(req,res,next){
+    res.locals.errors = null;
+    next();
+});
+
+
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(express.static(path.join(__dirname,'public')));
+
+//express Validator middleware
+app.use(expressValidator({
+    errorFormatter: function(param, msg, value) {
+        var namespace = param.split('.')
+        , root    = namespace.shift()
+        , formParam = root;
+  
+      while(namespace.length) {
+        formParam += '[' + namespace.shift() + ']';
+      }
+      return {
+        param : formParam,
+        msg   : msg,
+        value : value
+      };
+    }
+  }));
+
+app.get('/', function(req,res){
+
+    db.users.find(function (err, docs) {
+        //console.log(docs);
+        res.render('index',{
+            title:"customer",
+            users:docs
+        });
+    })
+});
+
+app.post('/users/add',function(req,res){
+
+    req.checkBody('first_name','First Name is Required').notEmpty();
+    req.checkBody('last_name','Last Name is Required').notEmpty();
+    req.checkBody('email','Email is Required').notEmpty();
+    req.getValidationResult().then(function(result){
+        if(!result.isEmpty()){
+
+           res.render('index',{
+                title:"customer",
+                errors:result.array()
+            });
+        }
+        else{
+            var newUser={
+                first_name:req.body.first_name,
+                last_name:req.body.last_name,
+                email:req.body.email
+            };
+            db.users.insert(newUser,function(err,result){
+                if(err){
+                    console.log(err);
+                }else{
+                    res.redirect('/'); 
+                }
+                
+            });
+        }
+    });
+});
+
+app.delete('/users/delete/:id',function(req,res){
+    db.users.remove({_id: ObjectId(req.params.id),function(err,result){
+        if(err){
+            console.log(err);
+        }
+        res.redirect('/');
+    }});
+})
+
+app.listen(3000,function(){
+    console.log('Server started on Port 3000');
+});
